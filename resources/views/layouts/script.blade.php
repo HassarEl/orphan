3b462cb76dfa6e3a631d6bb25aa5b41bf952cd1a
EL Mehdi HASSAR <mido.hass.31@gmail.com>
2023-05-03 15:51:07 +0100

    Create includ for script

---
+++ resources/views/layouts/script.blade.php
@@ -0,0 +1,49 @@
+    <!-- jQuery -->
+    <script src="{{ URL::asset('assets/vendor/jquery.min.js')}}"></script>
+
+    <!-- Bootstrap -->
+    <script src="{{ URL::asset('assets/vendor/popper.min.js')}}"></script>
+    <script src="{{ URL::asset('assets/vendor/bootstrap.min.js')}}"></script>
+
+    <!-- Perfect Scrollbar -->
+    <script src="{{ URL::asset('assets/vendor/perfect-scrollbar.min.js')}}"></script>
+
+    <!-- DOM Factory -->
+    <script src="{{ URL::asset('assets/vendor/dom-factory.js')}}"></script>
+
+    <!-- MDK -->
+    <script src="{{ URL::asset('assets/vendor/material-design-kit.js')}}"></script>
+
+    <!-- App -->
+    <script src="{{ URL::asset('assets/js/toggle-check-all.js')}}"></script>
+    <script src="{{ URL::asset('assets/js/check-selected-row.js')}}"></script>
+    <script src="{{ URL::asset('assets/js/dropdown.js')}}"></script>
+    <script src="{{ URL::asset('assets/js/sidebar-mini.js')}}"></script>
+    <script src="{{ URL::asset('assets/js/app.js')}}"></script>
+
+    <!-- App Settings (safe to remove) -->
+    <script src="{{ URL::asset('assets/js/app-settings.js')}}"></script>
+
+
+
+    <!-- Flatpickr -->
+    <script src="{{ URL::asset('assets/vendor/flatpickr/flatpickr.min.js')}}"></script>
+    <script src="{{ URL::asset('assets/js/flatpickr.js')}}"></script>
+
+    <!-- Global Settings -->
+    <script src="{{ URL::asset('assets/js/settings.js')}}"></script>
+
+    <!-- Chart.js -->
+    <script src="{{ URL::asset('assets/vendor/Chart.min.js')}}"></script>
+
+    <!-- App Charts JS -->
+    <script src="{{ URL::asset('assets/js/chartjs-rounded-bar.js')}}"></script>
+    <script src="{{ URL::asset('assets/js/charts.js')}}"></script>
+
+    <!-- Chart Samples -->
+    <script src="{{ URL::asset('assets/js/page.dashboard.js')}}"></script>
+
+    <!-- Vector Maps -->
+    <!-- <script src="assets/vendor/jqvmap/jquery.vmap.min.js"></script>
+<script src="assets/vendor/jqvmap/maps/jquery.vmap.world.js"></script>
+<script src="assets/js/vector-maps.js"></script> -->
